32238ec4681793ed2d396c445305b2b7621555d5
oracle <oracle@GBTC440R09RH.montgomerycollege.edu>
2017-05-10 21:31:38 -0400

    bullhorn project

---
+++ src/AddUser.java
@@ -0,0 +1,62 @@
+
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Random;
+import model.Bhuser;
+import customTools.DbUser;
+import java.util.Date;
+
+/**
+ * Servlet implementation class AddUser
+ */
+@WebServlet("/AddUser")
+public class AddUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public AddUser() {
+        super();
+    }
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		//This page does not require user to be logged in
+		String userName = request.getParameter("userName");
+		String userEmail = request.getParameter("userEmail");
+		String userPassword = request.getParameter("userPassword");
+		String userMotto = request.getParameter("userMotto");
+	 
+		String nextURL = "/error.jsp";
+		//check if user exists (by email)
+		Bhuser u = DbUser.getUserByEmail(userEmail);
+	
+		//create user and add them if they don't exit
+		if (u == null){
+			u = new Bhuser();
+			//u.setJoindate(dateFormat.format(date));
+			//u.setBhuserid(randomNum);
+			u.setUsername(userName);
+			u.setUseremail(userEmail);
+			u.setUserpassword(userPassword);
+			u.setMotto(userMotto);
+			DbUser.insert(u);
+			nextURL = "/home.jsp";
+		}else{
+			String message = "You have an account - ";
+			request.setAttribute("message", message);
+			nextURL = "/login.jsp";
+		}
+		//redirect to next page as indicated by the value of the nextURL variable
+		getServletContext().getRequestDispatcher(nextURL).forward(request,response);
+	}
+
+}
